d9b7716a78ac05a5f2d31ceba6ddf924a3334096
Amasick <2020eeb1152@iitrpr.ac.in>
2022-04-13 11:20:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Image Classification GUI 
+Image classification GUI app using Python.
+
+## Requirements
+1) Python 3
+2) Keras 
+3) Tensorflow
+4) PIL
+5) Numpy
+6) Matplotlib
+
+## Setup Instructions
+1) Clone the repository
+```python
+$ git clone https://github.com/kasai2210/image_classification_gui.git
+$ cd image_classification_gui
+```
+2) Install the dependencies
+#####  For Python 3 
+```bash
+$ pip3 install -r requirements.txt
+```
+#####  For Python
+```bash
+$ pip install -r requirements.txt
+```
+## Run the app
+#####  For Python 3 
+```bash
+$ python3 gui.py
+```
+#####  For Python 
+```bash
+$ python gui.py
+```
+## Testing on examples
+1) Click on upload image, upload an image of your choice
+2) Click the classify button
+
+YOU ARE GOOD TO GO !!
---
+++ gui.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image, ImageTk
+import numpy
+from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras as K
+from keras.models import model_from_json
+from keras.models import load_model
+
+model = load_model("model.h5")
+
+classes = { 
+    0:'aeroplane',
+    1:'automobile',
+    2:'bird',
+    3:'cat',
+    4:'deer',
+    5:'dog',
+    6:'frog',
+    7:'horse',
+    8:'ship',
+    9:'truck' 
+}
+
+top=tk.Tk()
+top.geometry('800x600')
+top.title('Image Classification CIFAR10')
+top.configure(background='#CDCDCD')
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((32,32))
+    image = numpy.expand_dims(image, axis=0)
+    image = numpy.array(image)
+    pred = model.predict([image])[0]
+    sign = classes[numpy.where(pred==1)[0][0]]
+    print(sign)
+    label.configure(foreground='#011638', text=sign)
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image", command=lambda: classify(file_path),padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white', font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25), (top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an image",command=upload_image, padx=10,pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial',10,'bold'))
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="Image Classification CIFAR10",pady=20, font=('arial',20,'bold'))
+
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
